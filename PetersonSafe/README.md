9f8cc2deac6189239579a2a7d102229544bca178
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-06 15:08:53 -0400

    Removed break statement problem.

--- PetersonSafe/README.md
+++ PetersonSafe/README.md
@@ -8,3 +8,8 @@ Author: Markus Kusano
 
 ## Notes
 Appears to timeout (more than 10 minutes)
+
+//Arijit
+The break statements seems to be placed wrongly because the section where the thread is iterating unless the lock is acquired there it should stay if it is not able to grab the lock.
+If it is not able to grab the lock after certain a= # of iteration then it should not jump into critical section. So I removed the break statements with return NULL.
+
--- PetersonSafe/main.c
+++ PetersonSafe/main.c
@@ -6,6 +6,8 @@
 #include <assert.h>
 //#define assert(e) if (!(e)) ERROR: goto ERROR;
 
+#define IT 10
+
 int flag1 = 0, flag2 = 0; // boolean flags
 int turn; // integer variable to hold the ID of the thread whose turn is it
 int x; // boolean variable to test mutual exclusion
@@ -17,7 +19,7 @@ void *thr1() {
   turn = 1;
 
   while (flag2==1 && turn==1) {
-      if (count++ > 1) break;
+      if (count++ > IT) return NULL;
   }
 
   // begin: critical section
@@ -33,7 +35,7 @@ void *thr2() {
   flag2 = 1;
   turn = 0;
   while (flag1==1 && turn==0) { 
-	if(count++ >1) break;
+	if(count++ >IT) return NULL;
   }
   // begin: critical section
   x = 1;
