ca52b243a83fd4f9252fde2c98b3b52f38702dd8
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-07 17:50:16 -0400

    Removed the thread specific locks.

--- HomeWork0UnSafe/main.c
+++ HomeWork0UnSafe/main.c
@@ -2,25 +2,20 @@
 #include <assert.h>
 #include <pthread.h>
 
-#include "hook.h"
-
 int m  = 0 ; 
 int IT = 2 ;
 
-pthread_mutex_t lock;
 
 void* inc(void *none) {
 	int i  = 0;
 	int temp = -1;
 	while(i < IT) {
-		pthread_mutex_lock(&lock);
 		if( m < 1000) {
 			temp = m;
 			m = temp +1;
 			m = IT;
 		}
 		++i;
-	        pthread_mutex_unlock(&lock);
 	}
 
 }
@@ -29,20 +24,17 @@ void* dec(void *none) {
 	int i  =0 ;
 	int temp = -1;
 	while(i < IT) {
-	        pthread_mutex_lock(&lock);
 		if(m > 0) {
 			temp  = m;
 			m = temp -1;		
 		}
 		++i;
-		pthread_mutex_unlock(&lock);
 	}
 }
 
 
 int main() {
 	pthread_t incr,decr;
-	pthread_mutex_init(&lock,0);	
 
 	pthread_create(&incr,NULL,inc,NULL);
 	pthread_create(&decr,NULL,dec,NULL);
@@ -50,6 +42,5 @@ int main() {
 	pthread_join(incr,NULL);
         pthread_join(decr,NULL);
 
-	pthread_mutex_destroy(&lock);
 	return 0;
 }
