553272ed3d8d892d77f678a51b45d739f8b11727
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-11 15:55:53 -0400

    Adding benchmark IncCas.

---
+++ IncCas/README.md
@@ -0,0 +1,2 @@
+This is SVCOMP 14
+02_inc_cas_true-unreach-label.c
---
+++ IncCas/main.c
@@ -0,0 +1,61 @@
+
+#include <pthread.h>
+
+#define THREAD_COUNT 2
+#define LOOP_COUNTER  5
+
+
+void  atomicCAS(
+  volatile unsigned *v,
+  unsigned e,
+  unsigned u,
+  unsigned *r)
+{
+	if(*v == e)
+	{
+		*v = u, *r = 1;
+	}
+	else
+	{
+		*r = 0;
+	}
+}
+
+volatile unsigned value;
+
+void* thr1(void* arg) {
+	unsigned v,vn,casret;
+	int counter = LOOP_COUNTER ;
+
+	do {
+		v = value;
+
+		if(v == 0u-1) {
+			return 0;
+		}
+
+		vn = v + 1;
+
+		 atomicCAS(&value,v,vn,&casret);
+		--counter;
+		if(counter <= 0) {
+			return 0;
+		}
+	}
+	while (casret==0);
+	return 0;
+}
+
+int main(){
+	pthread_t t[THREAD_COUNT];
+	int i;
+	for(i = 0 ; i < THREAD_COUNT; ++i) {
+		pthread_create(&t[i],0,thr1,0);
+	}
+
+	for(i = 0 ; i < THREAD_COUNT; ++i) {
+		pthread_join(t[i],0);
+	}
+        return 0;
+}
+
