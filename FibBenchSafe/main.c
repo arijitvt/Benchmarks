7b6e782316f53a66ed54773fc4300d4b30111a54
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-15 12:18:09 -0400

    Final changes before CS test

--- FibBenchSafe/main.c
+++ FibBenchSafe/main.c
@@ -1,6 +1,8 @@
 #include <pthread.h>
 #include <assert.h>
 
+#include "hook.h"
+
 int i=1, j=1;
 
 #define NUM 5
@@ -9,13 +11,20 @@ void *t1(void* arg)
 {
 	int k = 0;
 	int temp;
+	int orig;
 
 	for (k = 0; k < NUM; k++) {
 	      //  i+=j;
 	      //  The below code is converted to do the CS
+		orig = i;
 		temp = i;
 		temp = temp+j;
 		i = temp;
+		int result;
+		result = hook_assert( i == orig+j);
+		if( result == -1) {
+			return 0;
+		}
 	}
 	return NULL;
 }
@@ -24,13 +33,21 @@ void* t2(void* arg)
 {
 	int k = 0;
 	int temp;
+	int orig;
 
 	for (k = 0; k < NUM; k++) {
 		// j+=i;
 		// Equivalent code is below
+		orig = j;
 		temp = j;
 		temp = temp + i;
 		j = temp;
+		int result;
+		result = hook_assert( j == orig+i);
+		if(result == -1) {
+			return 0;
+		}
+
 	}
 	return NULL;
 }
