b630220bb4093e8111ca8a75d1fd150381d58bf2
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-14 22:10:47 -0400

    Modified for CS.

--- Sync01_Safe/main.c
+++ Sync01_Safe/main.c
@@ -2,45 +2,53 @@
 #include <pthread.h>
 #include <assert.h>
 
-int  num;
+int  num = 1;
 
 pthread_mutex_t  m;
 pthread_cond_t  empty, full;
 
 void * thread1(void * arg)
 {
-  pthread_mutex_lock(&m);
+	int temp;
+	pthread_mutex_lock(&m);
 
-  while (num > 0) 
-    pthread_cond_wait(&empty, &m);
-  
-  num++;
+	while (num > 0) 
+		pthread_cond_wait(&empty, &m);
+	
+	//num++;
+	//Eq is below
+	temp = num;
+	temp = temp+1;
+	num = temp;
 
-  pthread_mutex_unlock(&m);
-  pthread_cond_signal(&full);
+	pthread_mutex_unlock(&m);
+	pthread_cond_signal(&full);
 }
 
 
 void * thread2(void * arg)
 {
-  pthread_mutex_lock(&m);
+	int temp;
+	pthread_mutex_lock(&m);
 
-  while (num == 0) 
-    pthread_cond_wait(&full, &m);
+	while (num == 0) 
+		pthread_cond_wait(&full, &m);
 
-  num--;
-  
-  pthread_mutex_unlock(&m);
+	//  num--;
+	//  Eq is below
 
-  pthread_cond_signal(&empty);
+	temp = num;
+	temp = temp -1;
+	num = temp;
+
+	pthread_mutex_unlock(&m);
+	pthread_cond_signal(&empty);
 }
 
 
 int main()
 {
   pthread_t  t1, t2;
-
-  num = 1;
 
   pthread_mutex_init(&m, 0);
   pthread_cond_init(&empty, 0);
@@ -52,12 +60,6 @@ int main()
   pthread_join(t1, 0);
   pthread_join(t2, 0);
 
-  if (num!=1)
-  {
-    //ERROR: goto ERROR;
-    //;
-   // assert(0);
-  }
 
   return 0;
   
