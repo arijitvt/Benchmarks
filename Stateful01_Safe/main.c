1885058756ef7e1f92d3d0d18632cafa8f924534
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-15 12:29:25 -0400

    Added changes to main for the final CS

--- Stateful01_Safe/main.c
+++ Stateful01_Safe/main.c
@@ -1,6 +1,8 @@
 #include <pthread.h> 
 #include <assert.h>
 
+#include "hook.h"
+
 pthread_mutex_t  ma, mb;
 int data1 = 10;
 int data2 = 10;
@@ -8,21 +10,37 @@ int data2 = 10;
 void * thread1(void * arg)
 {
 	int temp;
+	int orig;
+	int result;
 	pthread_mutex_lock(&ma);
 	//data1++;
 	//Eqivalent is below
+	orig = data1;
 	temp = data1;
 	temp = temp+1;
 	data1= temp;
+	
+	result = hook_assert( data1 == orig+1);
+	if(result == -1) {
+		return 0;
+	}
+	
 
 	pthread_mutex_unlock(&ma);
 
 	pthread_mutex_lock(&ma);
 	//  data2++;
 	//  Equivalent is below
+	orig = data2;
 	temp = data2;
 	temp = temp+1;
 	data2= temp;
+	
+	result = hook_assert( data2 == orig+1);
+	if(result == -1) {
+		return 0;
+	}
+	
 	pthread_mutex_unlock(&ma);
 }
 
@@ -30,20 +48,35 @@ void * thread1(void * arg)
 void * thread2(void * arg)
 {  
 	int temp;
+	int orig;
+	int result;
 	pthread_mutex_lock(&ma);
 	//  data1+=5;
 	//  Equivalent is below
+        orig = data1;
 	temp = data1;
 	temp = temp +5;
 	data1 = temp;
+
+	result = hook_assert( data1 == orig+5);
+	if(result == -1) {
+		return 0;
+	}
+
 	pthread_mutex_unlock(&ma);
 
 	pthread_mutex_lock(&ma);
 	//  data2-=6;
 	//  Equivalent is below
+	orig = data2;
 	temp = data2;
 	temp = temp -6;
 	data2 = temp;
+	
+	result = hook_assert( data2 == orig-6);
+	if(result == -1) {
+		return 0;
+	}
 	pthread_mutex_unlock(&ma);
 }
 
