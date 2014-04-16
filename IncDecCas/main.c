510813d817b2e521224b6727934f95b10ca59e2b
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-16 15:53:03 -0400

    changes for the cs

--- IncDecCas/main.c
+++ IncDecCas/main.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include "hook.h"
 
 #define LOOP_COUNT 5
 #define THREAD_COUNT 2 
@@ -33,17 +34,20 @@ void __VERIFIER_atomic_CAS(
 void* inc(void *arg) {
 	unsigned inc__v, inc__vn, inc__casret;
 	int loopCounter = LOOP_COUNT;
+	int orig;
+	
 
 	do {
 		inc__v = value;
+                orig  = value;
 
 		if(inc__v == 0u-1) {
 			return 0; /*increment failed, return min*/
 		}
-
 		inc__vn = inc__v + 1;
 
 		__VERIFIER_atomic_CAS(&value,inc__v,inc__vn,&inc__casret,&inc_flag);
+		hook_assert(value == orig + 1);
 		loopCounter--;
 		if(loopCounter <= 0) {
 			return 0;
@@ -57,9 +61,11 @@ void* inc(void *arg) {
 void* dec(void *arg) {
 	unsigned dec__v, dec__vn, dec__casret;
 	int loopCounter = LOOP_COUNT;
+	int orig;
 	
 	do {
 		dec__v = value;
+		orig = value;
 
 		if(dec__v == 0) {
 			return NULL; /*decrement failed, return max*/
@@ -68,6 +74,7 @@ void* dec(void *arg) {
 		dec__vn = dec__v - 1;
 
 		__VERIFIER_atomic_CAS(&value,dec__v,dec__vn,&dec__casret,&dec_flag);
+		hook_assert(value == orig-1);
 
 		loopCounter--;
 		if(loopCounter <= 0) {
