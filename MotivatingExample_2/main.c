1b0b9fdcb8210d812997a850ad2d90012d679bb8
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-18 11:18:02 -0400

    Adding the main.c file.

--- MotivatingExample_2/main.c
+++ MotivatingExample_2/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#include "hook.h"
+
 pthread_mutex_t lock;
 int balance = 0 ;
 
@@ -33,20 +35,36 @@ void*  deposit(void *arg) {
 }
 
 void* withdraw(void *arg) {
-	int bal = getBalance();
+	int bal = balance;
 	bal = bal -100;
 	setBalance(bal);
+	hook_assert(bal == (getBalance() - 100));
+}
+
+void *transfer(void *arg) {
+	int temp = getBalance();
+	deposit(0);
+	withdraw(0);
+	hook_assert(temp == getBalance());
 }
 
 
 int main() {
 	pthread_t w,d;
-	pthread_create(&d,0,deposit,0);
-	pthread_create(&w,0,withdraw,0);
+	//pthread_create(&d,0,deposit,0);
+	//pthread_create(&w,0,withdraw,0);
 
+	pthread_mutex_init(&lock,0);
+
+	//pthread_create(&d,0,transfer,0);
+	//pthread_create(&d,0,transfer,0);
+	pthread_create(&d,0,withdraw,0);
+	pthread_create(&w,0,withdraw,0);
+	
 	pthread_join(d,0);
 	pthread_join(w,0);
 
+	pthread_mutex_destroy(&lock);
 	return 0;
 
 }
