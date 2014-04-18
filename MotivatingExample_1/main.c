c53f0a1d28e59e9ac6de8a2db769f5d599b8a227
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-17 22:52:48 -0400

    Main is implemented

--- MotivatingExample_1/main.c
+++ MotivatingExample_1/main.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+
+#include "hook.h"
+
+struct Example{
+	int a;
+	int b;
+	int c;
+};
+
+typedef struct Example Example;
+
+Example *ex;
+
+void* thr1(void *arg) {
+	if(ex != NULL) {
+		ex->a = 100;
+		hook_assert(ex->a == 100);
+	}
+}
+
+void* thr2(void *arg) {
+	ex = (Example*)malloc(sizeof(Example));
+}
+
+int main() {
+	pthread_t t1,t2;
+
+	pthread_create(&t1,0,thr1,0);
+	pthread_create(&t2,0,thr2,0);
+
+	pthread_join(t1,0);
+	pthread_join(t2,0);
+
+	return 0;
+}
