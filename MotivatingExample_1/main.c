7c8742e8ece0ebc8b44c76cf120427b101d9370a
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-17 23:03:46 -0400

    final implementation of main.c

--- MotivatingExample_1/main.c
+++ MotivatingExample_1/main.c
@@ -15,9 +15,23 @@ typedef struct Example Example;
 Example *ex;
 
 void* thr1(void *arg) {
+	int result = 0 ;
 	if(ex != NULL) {
 		ex->a = 100;
-		hook_assert(ex->a == 100);
+		result = hook_assert(ex->a == 100);
+		if(result == -1) {
+			return 0;
+		}
+		ex->b = 200;
+		result = hook_assert(ex->a == 100 && ex->b == 200);
+		if(result == -1) {
+			return 0;
+		}
+		ex->c= 300;
+		result = hook_assert(ex->a == 100 && ex->b== 200 && ex->c == 300);
+		if(result == -1) {
+			return 0;
+		}
 	}
 }
 
