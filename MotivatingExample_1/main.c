55a6e80a1d23baefc6abd2bbf85661428eda88b8
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-19 01:16:35 -0400

    Adding paper results.

--- MotivatingExample_1/main.c
+++ MotivatingExample_1/main.c
@@ -1,31 +1,39 @@
-struct Example{
-	int a,b,c;
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "hook.h"
+
+struct 
+Example{int a,b,c;};
 typedef struct Example Example;
 Example *ex;
 void* thr1(void *arg) {
-	int result = 0 ;
 	if(ex != NULL) {
 		ex->a = 100;
-		result = hook_assert(ex->a == 100);
-		if(result == -1) {
-			return 0;
-		}
+		hook_assert(ex->a == 100);
 		ex->b = 200;
-		result = hook_assert(ex->a == 100 
+		hook_assert(ex->a == 100 
 				&& ex->b == 200);
-		if(result == -1) {
-			return 0;
-		}
 		ex->c= 300;
-		result = hook_assert(ex->a == 100 && 
-				ex->b== 200 
-				&& ex->c == 300);
-		if(result == -1) {
-			return 0;
-		}
+		hook_assert(ex->a == 100 
+				&& ex->b== 200 && 
+				ex->c == 300);
 	}
 }
+
 void* thr2(void *arg) {
 	ex = (Example*)malloc(sizeof(Example));
+}
+
+int main() {
+	pthread_t t1,t2;
+
+	pthread_create(&t1,0,thr1,0);
+	pthread_create(&t2,0,thr2,0);
+
+	pthread_join(t1,0);
+	pthread_join(t2,0);
+
+	return 0;
 }
