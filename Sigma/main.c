ab4a5012d1753e7c18bc7597f49f58fdf1eb1caf
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-21 12:40:19 -0400

    Main has to be like this

--- Sigma/main.c
+++ Sigma/main.c
@@ -16,7 +16,6 @@ int array_index;
 void *thread(void * arg)
 {
 	int temp = array_index;
-	sleep(rand()%2+1);
 	array[temp] = 1;
 	return 0;
 }
@@ -26,7 +25,6 @@ int main()
 {
 	int tid, sum;
 	pthread_t *t;
-	srand(time(0));
 
 	t = (pthread_t *)malloc(sizeof(pthread_t) * SIGMA);
 	array = (int *)malloc(sizeof(int) * SIGMA);
