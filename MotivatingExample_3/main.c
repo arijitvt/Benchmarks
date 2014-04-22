0fe249579284c580b976277cdae490819031d72e
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-04-21 23:47:08 -0400

    Adding results

--- MotivatingExample_3/main.c
+++ MotivatingExample_3/main.c
@@ -3,29 +3,22 @@
 #include <time.h>
 #include "hook.h"
 
-struct Example { int a, b, c};
-typedef struct Example Example;
-
-Example A[128] ;
+typedef struct Example { int a, b, c;}Example;
+Example *A[128] ;
 Example *p = NULL;
-
-
-void* thr1 (void *) {
-  tmp = p;
+void* thr1 (void *arg) {
+  Example *tmp = p;
   if (tmp != NULL) {
     p->a = 100;
     p->b = 200;
     p->c = 300;
-    
     hook_assert( tmp->a == 100 && tmp->b == 200 && tmp->c == 300 );
   }
   return 0;
 }
-
-
-void * thr2(void *) {
-  i = rand() % 127+1;
-  p = & A[i];
+void * thr2(void *arg) {
+  int i = rand() % 127+1;
+  p =  A[i];
   return 0;
 }
 
@@ -34,6 +27,9 @@ int main() {
 	int i;
 	for(i = 0 ; i < 128;++i) {
 		A[i] = (Example *) malloc(sizeof(Example));
+		A[i]->a = 0 ;
+		A[i]->b = 0 ;
+		A[i]->c = 0 ;
 	}
 	p = A[0];
 	pthread_t t1,t2;
