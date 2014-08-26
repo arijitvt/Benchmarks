c1ad352b837a447d9d8b37d95184f6576e6ca229
Markus Kusano <mukusano@vt.edu>
2014-08-26 16:25:55 -0400

    annotate accountbad main with inv.

--- AccountBad/main.c
+++ AccountBad/main.c
@@ -1,3 +1,6 @@
+/*
+ * See the bottom of this file for an example daikon output
+ */
 #include <pthread.h>
 #include <stdio.h>
 #include <assert.h>
@@ -140,6 +143,51 @@ void *withdraw(void *arg)
 }
 
 
+/*
+ * 
+..check_result():::ENTER
+True: since check result starts first, both are set to 0
+::deposit_done == ::withdraw_done
+True: balance is initialized by main to x
+::balance == ::x
+::deposit_done == 0
+::balance == 1
+constants
+::y == 2
+::z == 4
+const
+arg == null
+===========================================================================
+..check_result():::EXIT
+constants
+::y == orig(::y)
+::z == orig(::z)
+::x == orig(::balance)
+::x == orig(::x)
+return == orig(arg)
+true depends on the ordering of the other threads
+::deposit_done one of { 0, 1 }
+::withdraw_done one of { 0, 1 }
+true, never zero
+::balance != 0
+::y == 2
+::z == 4
+::x == 1
+return == null
+::deposit_done != ::balance
+::deposit_done < ::y
+::deposit_done < ::z
+::deposit_done <= ::x
+::deposit_done >= orig(::deposit_done)
+::withdraw_done != ::balance
+::withdraw_done < ::y
+::withdraw_done < ::z
+::withdraw_done <= ::x
+::withdraw_done >= orig(::deposit_done)
+::balance != ::y
+::balance < ::z
+::balance != orig(::deposit_done)
+*/
 void *check_result(void *arg) 
 {
   pthread_mutex_lock(&m);
@@ -152,6 +200,26 @@ void *check_result(void *arg)
   return NULL;
 }
 
+/*
+ *
+..main():::ENTER
+::deposit_done == ::withdraw_done
+::deposit_done == ::balance
+::deposit_done == ::y
+::deposit_done == ::z
+::deposit_done == ::x
+::deposit_done == 0
+===========================================================================
+..main():::EXIT
+::deposit_done == ::withdraw_done
+::deposit_done == ::x
+::deposit_done == 1
+::balance == -1
+const
+::y == 2
+::z == 4
+return == 99
+*/
 int main() 
 {
   pthread_t t1, t2, t3;
@@ -171,5 +239,175 @@ int main()
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
 
+  assert(balance == -1);
+
   return 99;
 }
+/*
+ * Daikon output
+ * Number of invariants: 78
+ * Incorrect invariants: 22
+ *
+===========================================================================
+..check_result():::ENTER
+::deposit_done == ::withdraw_done
+True: const
+::x == 1
+True: const
+::y == 2
+True: const
+::z == 4
+False: should be 1
+::balance == -1
+False: could be false
+::deposit_done == true
+True, const
+arg == null
+===========================================================================
+..check_result():::EXIT
+True: const
+::x == orig(::x)
+True: const
+::y == orig(::y)
+True: const
+::z == orig(::z)
+False: could be modified
+::balance == orig(::balance)
+False
+::deposit_done == ::withdraw_done
+False
+::deposit_done == orig(::deposit_done)
+False
+::deposit_done == orig(::withdraw_done)
+True const
+return == orig(arg)
+True const
+::x == 1
+True const
+::y == 2
+True const
+::z == 4
+False: depends on interleaving
+::balance == -1
+False: depends on interleaving
+::deposit_done == true
+return == null
+===========================================================================
+..deposit():::ENTER
+False: depends on interleaving
+::x == ::balance
+False: depends on interleaving (withdraw could be 0
+::deposit_done == ::withdraw_done
+True; const
+::x == 1
+True; const
+::y == 2
+True; const
+::z == 4
+True: set by this func
+::deposit_done == false
+True: const
+arg == null
+===========================================================================
+..deposit():::EXIT
+True: const
+::x == orig(::x)
+False: depends on interleaving
+::x == orig(::balance)
+True: const
+::y == orig(::y)
+True: const
+::z == orig(::z)
+False: interleaving
+::withdraw_done == orig(::deposit_done)
+False: interleaving
+::withdraw_done == orig(::withdraw_done)
+True: const
+return == orig(arg)
+True: const
+::x == 1
+True: const
+::y == 2
+True: const
+::z == 4
+False: interleaving
+::balance == 3
+False: interleaving
+::deposit_done == true
+False: interleaving
+::withdraw_done == false
+True: const
+return == null
+===========================================================================
+..main():::ENTER
+True: const
+::x == ::y
+True: const
+::x == ::z
+True: const
+::x == ::balance
+True: const
+::deposit_done == ::withdraw_done
+True: const
+::x == 0
+True: const
+::deposit_done == false
+===========================================================================
+..main():::EXIT
+True:
+::deposit_done == ::withdraw_done
+True:
+::x == 1
+True:
+::y == 2
+True:
+::z == 4
+True:
+::balance == -1
+True:
+::deposit_done == true
+True:
+return == 99
+===========================================================================
+..withdraw():::ENTER
+True: const
+::x == 1
+True: const
+::y == 2
+True: const
+::z == 4
+False: interleaving
+::balance == 3
+False: interleaving
+::deposit_done == true
+True: set by this thread
+::withdraw_done == false
+True: const
+arg == null
+===========================================================================
+..withdraw():::EXIT
+True: const
+::x == orig(::x)
+True: const
+::y == orig(::y)
+True: const
+::z == orig(::z)
+False: interleaving
+::deposit_done == ::withdraw_done
+False: interleaving
+::deposit_done == orig(::deposit_done)
+True: const
+return == orig(arg)
+True: const
+::x == 1
+True: const
+::y == 2
+True: const
+::z == 4
+False: interleaving
+::balance == -1
+False: interleaving
+::deposit_done == true
+True: const
+return == null
+ */
