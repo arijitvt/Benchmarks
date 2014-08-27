64146b64a42fd726f2744d89c932645c46faf325
Markus Kusano <mukusano@vt.edu>
2014-08-27 16:15:26 -0400

    add invariant annotations to main for IncDec

--- IncDec/main.c
+++ IncDec/main.c
@@ -389,3 +389,216 @@ int main(){
 	return 0;
 }
 
+/*
+ * Example Daikon Output
+ *
+ * Num invariants: 94
+ * Num true: 37
+ * Num false: 57
+===========================================================================
+..__VERIFIER_atomic_acquire():::ENTER
+False: m could be either 1 or 0 depending on if another thread acquired the lock already
+::value == ::m
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::inc_flag
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::dec_flag
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::inc_v
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::dec_v
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == 0
+===========================================================================
+..__VERIFIER_atomic_acquire():::EXIT
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::inc_flag
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::dec_flag
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::inc_v
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == ::dec_v
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == orig(::value)
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == orig(::m)
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == orig(::inc_flag)
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == orig(::dec_flag)
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == orig(::inc_v)
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == orig(::dec_v)
+False: Value can be either 0 or 1 when a thread enters acquire since the other thread could have performed a modification
+::value == 0
+1. True: m is always set to one on exit
+::m == 1
+===========================================================================
+..__VERIFIER_atomic_release():::ENTER
+::value == ::inc_flag
+False: decrement is only sometimes performed
+::dec_flag == ::inc_v
+2. True: dec_flag always mirrors dec_v
+::dec_flag == ::dec_v
+3. True: value is either incremented or decrmented
+::value one of { 0, 1 }
+False: another thread could have modified the mutex
+::m == 1
+False: dec_flag is sometimes 1
+::dec_flag == 0
+===========================================================================
+..__VERIFIER_atomic_release():::EXIT
+False: value can be zero or one
+::value == ::inc_flag
+False: value can change in this function
+::value == orig(::value)
+False: value can be zero or one
+::value == orig(::inc_flag)
+False: dec flag can be zero or one
+::m == ::dec_flag
+4. True: inc_v is always 0
+::m == ::inc_v
+False: dec_v is sometimes 1
+::m == ::dec_v
+False: dec_flag can change in this function
+::m == orig(::dec_flag)
+False: inc_v can change in this function
+::m == orig(::inc_v)
+False: dec_v can change in this function
+::m == orig(::dec_v)
+5. True: value could be either zero or one
+::value one of { 0, 1 }
+6. True: m is always released
+::m == 0
+===========================================================================
+..dec():::ENTER
+False: value can be one
+::value == ::m
+False: value can be one
+::value == ::inc_flag
+False: value can be one
+::value == ::dec_flag
+False: value can be one
+::value == ::inc_v
+False: value can be one
+::value == ::dec_v
+False: value can be one
+::value == 0
+7. True: const
+arg == null
+===========================================================================
+..dec():::EXIT
+False: value can be zero or one
+::value == ::m
+False: value can be zero or one
+::value == ::inc_flag
+False: value can be zero or one
+::value == ::dec_flag
+False: value can be zero or one
+::value == ::inc_v
+False: value can be zero or one
+::value == ::dec_v
+False: value can be zero or one
+::value == orig(::value)
+False: value can be zero or one
+::value == orig(::m)
+False: value can be zero or one
+::value == orig(::inc_flag)
+False: value can be zero or one
+::value == orig(::dec_flag)
+False: value can be zero or one
+::value == orig(::inc_v)
+False: value can be zero or one
+::value == orig(::dec_v)
+False: value can be zero or one
+::value == 0
+8. True: const
+return has only one value
+===========================================================================
+..inc():::ENTER
+False: m can be zero or one
+::value == ::m
+9. True: value is zero on entry
+::value == ::inc_flag
+10. True: value is zero on entry
+::value == ::dec_flag
+11. True: value is zero on entry
+::value == ::inc_v
+12. True: value is zero on entry
+::value == ::dec_v
+13. True: value is zero on entry
+::value == 0
+14. True: const
+arg == null
+===========================================================================
+..inc():::EXIT
+::value == ::inc_flag
+False: dec_flag can be zero or one
+::m == ::dec_flag
+15. True: inc_v is always zoer
+::m == ::inc_v
+False: dec_ can be zero or one
+::m == ::dec_v
+16. True: == 0
+::m == orig(::value)
+False: m could be one on entry
+::m == orig(::m)
+17. True: inc_flag == 0
+::m == orig(::inc_flag)
+18. True: dec_flag == 0 on entry
+::m == orig(::dec_flag)
+19. True:
+::m == orig(::inc_v)
+20. True:
+::m == orig(::dec_v)
+False: value can be zero or one
+::value == 1
+21. True: m is always released
+::m == 0
+22. True: const
+return has only one value
+===========================================================================
+..main():::ENTER
+23. True: init
+::value == ::m
+24. True: init
+::value == ::inc_flag
+25. True: init
+::value == ::dec_flag
+26. True: init
+::value == ::inc_v
+27. True: init
+::value == ::dec_v
+28. True: init
+::value == 0
+===========================================================================
+..main():::EXIT
+False: zero or one
+::value == ::inc_flag
+False: flag 0 or 1
+::m == ::dec_flag
+29. True: inc_v == 0
+::m == ::inc_v
+False: dec_v = 0 or 1
+::m == ::dec_v
+30. True:
+::m == orig(::value)
+31. True:
+::m == orig(::m)
+32. True:
+::m == orig(::inc_flag)
+33. True:
+::m == orig(::dec_flag)
+34. True:
+::m == orig(::inc_v)
+35. True:
+::m == orig(::dec_v)
+False: 0 or 1
+::value == 1
+36. True: always released
+::m == 0
+37. True: const
+return == 0
+ */
