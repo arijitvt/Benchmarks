0d56027365f1bc95a78097f7ca523435c1ba641c
Markus Kusano <mukusano@vt.edu>
2014-08-27 16:52:47 -0400

    add invariant annotations

--- IncTrue/main.c
+++ IncTrue/main.c
@@ -23,6 +23,54 @@ volatile unsigned m = 0;
 }                                   \
 
 
+/*
+ * ==== Thread 2 Invariants ====
+ *
+===========================================================================
+..thr1():::ENTER
+True: thread 1 goes first (init)
+::value == ::m
+True: thread 1 goes first (init)
+::value == 0
+True: const
+arg == null
+===========================================================================
+..thr1():::EXIT
+True: always released
+::m == orig(::value)
+True: always released
+::m == orig(::m)
+True: const
+return == orig(arg)
+True: 1 inc or 2
+::value one of { 1, 2 }
+True: always released
+::m == 0
+True: const
+return == null
+True: 1 inc or 2
+::value > ::m
+* =======
+*
+* ==== Thread 2 Inv ====
+===========================================================================
+..thr1():::ENTER
+True: thread 1 goes firs
+::value one of { 0, 1 }
+::m one of { 0, 1 }
+arg == null
+===========================================================================
+..thr1():::EXIT
+return == orig(arg)
+::value one of { 1, 2 }
+::m == 0
+return == null
+::value > ::m
+::value > orig(::value)
+::value >= orig(::m)
+::m <= orig(::value)
+::m <= orig(::m)
+*/
 void * thr1(void* arg) {
 	unsigned v = 0;
 
@@ -41,6 +89,29 @@ void * thr1(void* arg) {
 	}
 }
 
+/*
+ *
+===========================================================================
+..main():::ENTER
+True: init
+::value == ::m
+True: init
+::value == 0
+===========================================================================
+..main():::EXIT
+True: always released
+::m == return
+True: always released
+::m == orig(::value)
+True: always released
+::m == orig(::m)
+True: Either one inc or two
+::value one of { 1, 2 }
+True: always released
+::m == 0
+True: Either one inc or two
+::value > ::m
+*/
 int main(){
 	pthread_t t[BIG];
 	int i;
@@ -53,3 +124,50 @@ int main(){
 	return 0;
 }
 
+/*
+ *
+ * Daikon output
+ * Num Inv: 15
+ * Num true: 12
+ * Num False: 3
+ *
+===========================================================================
+True: init
+..main():::ENTER
+True: init
+::value == ::m
+True: init
+::value == 0
+===========================================================================
+..main():::EXIT
+True: released
+::m == orig(::value)
+True: released
+::m == orig(::m)
+False: 1 or 2 incs
+::value == 2
+True: released
+::m == 0
+True: const
+return == 0
+===========================================================================
+..thr1():::ENTER
+True: 1 inc or 2
+::value one of { 0, 1 }
+False: m can be one at the start
+::m == 0
+True: const
+arg == null
+===========================================================================
+..thr1():::EXIT
+False: m can be 1 at the start
+::m == orig(::m)
+True: const
+return == orig(arg)
+True: 1 inc or 2
+::value one of { 1, 2 }
+True: released
+::m == 0
+True: const
+return == null
+*/
