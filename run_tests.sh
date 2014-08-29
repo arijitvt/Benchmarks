178a4eeab5c89645e6f430046b3fe3c18d96ca9f
Markus Kusano <mukusano@vt.edu>
2014-08-29 16:31:02 -0400

    add all current tests to testing script

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,33 @@
 # Run all the tests
 
 
-TESTS="AccountBad LogProcSweep Szymanski Reorder Lazy01Safe LogProcSweep IncCas IncDec IncTrue IncDecCas"
+TESTS="AccountBad \
+DekkerSafe \
+FibBenchSafe
+IncCas \
+IncDec \
+IncDecCas
+IncTrue \
+LamportSafe \
+LogProcSweep \
+PetersonSafe
+Reorder \
+Stateful01_Safe \
+Sync01_Safe \
+Szymanski \
+TimeVarMutexSafe \
+Lazy01Safe \
+LogProcSweep"
+
+# check to make sure each directory actually exists
+for f in $TESTS
+do
+  if [ ! -d $f ]
+  then
+    echo "Error: Directory not found: $f"
+  fi
+done
+
 
 # secret feature: pass clean as argument one to delete all the output files
 if [ "$1" = "clean" ]
